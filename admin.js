a7ee5749f36fa0ce75a7269488fce7afd1a9652b
imbryan140 <imbryan140@gmail.com>
2026-01-17 22:16:57 -0400

    admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Configuración de Firebase actualizada
 const firebaseConfig = {
   apiKey: "AIzaSyBBQlkviaNN4g7-2KEcSPogZFyz4ejTX98",
   authDomain: "mensaje-anonimo-be30f.firebaseapp.com",
